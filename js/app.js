e3634b4f6f60d4ab58ac44f11cc9930ac9de406f
andela-lozidu <lois.ozidu@andela.co>
2015-04-08 07:45:53 +0100

    commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-var textToSpeech= {
+var textToSpeech = {
   textField: null,
   convertButton: null,
+  resetButton: null,
   audioResultDisplay: null,
 
   url: "http://api.voicerss.org/",
@@ -11,33 +12,38 @@ var textToSpeech= {
   },
 
   init: function() {
-    textToSpeech.textField = $("#textToConvert");
-    textToSpeech.convertButton = $("#convertButton");
-    textToSpeech.audioResultDisplay = $("#audioSrc");
-    textToSpeech.initEvent();
+    textToSpeech.textField = $("#textToConvert"); //defines what textField represents
+    textToSpeech.convertButton = $("#convertButton"); //defines what convertButton represents
+    textToSpeech.audioResultDisplay = $("#audioSrc"); //defines what audioResultDisplay represents
+    textToSpeech.initEvent(); 
   },
+
+  //function that describes what should happen on click of the convertButton
   initEvent: function() {
     textToSpeech.convertButton.click(textToSpeech.validate);
   },
+
+  //checks the input string to see if there was text typed(validation process)
   validate: function(e) {
     e.preventDefault();
+
     var convertText = textToSpeech.textField.val();
     if(convertText !== "") {
       textToSpeech.params.src = convertText;
       textToSpeech.convert();
     }
     else {
-      alert("Yor browser does not support this");
+      alert("Please type in text that you want to convert");
     }
   },
 
+  //function that describes the convertion from text to audio
   convert: function() {
     var audioBook = '<audio controls autoplay>';
     audioBook += '<source src =' + textToSpeech.url + '?key=' + textToSpeech.params.key + '&src=' + encodeURIComponent(textToSpeech.params.src) +'&hl=' + textToSpeech.params.hl + '>';
-    audioBook += "Your browser does not support this";
+    audioBook += "Please type in text that you want to convert";
     audioBook += '</audio>';
     textToSpeech.audioResultDisplay.html(audioBook);
   }
 }
 $(document).ready(textToSpeech.init);
-//});
